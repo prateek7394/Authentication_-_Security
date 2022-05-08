ed8fcf429ec24f980ac9ef4a8d508542b7d448cb
Prateek Shukla <200108043@hbtu.ac.in>
2022-05-08 20:45:49 +0530

    Using passport.js

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+// 3 new packages
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,19 +18,33 @@ app.use(bodyParser.urlencoded({
     extended:true
 }));
 
-main().catch(err => console.log(err));
+app.use(session({
+    secret: "Kuchh bhi likh do yaha pe",
+    resave: false,
+    saveUninitialized: false
+}));
 
-async function main(){
-    await mongoose.connect("mongodb://localhost:27017/userDB")
-}
+// Initialise passport
+app.use(passport.initialize());
+
+// Use passport to initialise session
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB")
 
 const userSchema = new mongoose.Schema({
     email: String,
     password : String
 });
 
-
+userSchema.plugin(passportLocalMongoose);
+ 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home");
@@ -38,57 +55,58 @@ app.get("/login", function(req,res){
 app.get("/register", function(req,res){
     res.render("register");
 })
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+
+// logout method of passport is used
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 
 
 app.post("/register", function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        // Store hash in your password DB.
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save(function(err){
-            if(!err){
-                res.render("secrets");
-            }
-            else{
-                res.render(err);
-            }
-        });
+//  use method provided by passportpackage
+    User.register({username:req.body.username}, req.body.password, function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req, res , function(){
+        //  since the users are being authenticated and a login session is saved for them in the cookies,
+        //  therefore they are redirected to the secrets route if they were previously logged in
+                res.redirect("/secrets");
+            });
+        }
     });
-
-})
+});
 
 app.post("/login", function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
-    User.findOne({email: username}, function(err, foundUser){
+    // Here login function provided by passport is used
+    req.login(user, function(err){
         if(err){
             console.log(err);
         }
         else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result == true){
-                        res.render("secrets");
-                    }
-                    else{
-                        res.send("Password not matched!")
-                    }
-                });
-               
-            }
-            else{
-                res.send("User not found!!")
-            }
-
+            passport.authenticate("local")(req, res , function(){
+                res.redirect("/secrets");
+            });
         }
-    })
-
-})
+    });
+});
 
 app.listen(3000,function(){
     console.log("Server running on port 3000!")
